e6a4240675bf2f83865e67c1b6c9edac96985e61
stanley92 <Stanley92@hotmail.com>
2015-08-22 23:37:40 +0800

    Implemented Filter.java

--- src/filters/Filter.java
+++ src/filters/Filter.java
@@ -1,4 +1,31 @@
 package filters;
 
+import java.io.EOFException;
+import pipe.Pipe;
+
 public class Filter {
+	
+	protected Pipe inPipe;
+	protected Pipe outPipe;
+	
+	public void setIn(Pipe data) {
+		inPipe = data;
+	}
+	
+	public void setOut(Pipe data) {
+		outPipe = data;
+	}
+	
+	public void write(String string) {
+		if (string == null) {
+			outPipe.close();
+			return;
+		}
+		outPipe.write(string);
+	}
+	
+	public String read() throws EOFException {
+		return inPipe.read();
+	}
+
 }
