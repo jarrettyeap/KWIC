5814e7312f774cdb845758a3e76f6e88ef4de1cf
Hooi Tong <hooitongx@gmail.com>
2015-08-24 22:38:55 +0800

    Setup skeleton and API definition for `ConsoleInput` and `ConsoleOutput`

--- src/generator/ConsoleInput.java
+++ src/generator/ConsoleInput.java
@@ -1,4 +1,22 @@
 package generator;
 
-public class ConsoleInput {
+import pipe.Pipe;
+
+import java.util.List;
+
+public class ConsoleInput implements Runnable {
+    private Pipe<List<String>> inputPipe;
+
+    public ConsoleInput(Pipe<List<String>> input) {
+        this.inputPipe = input;
+    }
+
+    @Override public void run() {
+        putInputIntoPipe();
+    }
+
+    private void putInputIntoPipe() {
+        // Read Input
+        // Write into Pipe
+    }
 }
--- src/sink/ConsoleOutput.java
+++ src/sink/ConsoleOutput.java
@@ -1,4 +1,36 @@
 package sink;
 
-public class ConsoleOutput {
+import pipe.Pipe;
+
+import java.util.List;
+
+public class ConsoleOutput implements Runnable {
+    private Pipe<List<String>> outputPipe;
+
+    public ConsoleOutput(Pipe<List<String>> output) {
+        this.outputPipe = output;
+    }
+
+    @Override public void run() {
+        receiveOutputFromPipe();
+    }
+
+    private void receiveOutputFromPipe() {
+        try {
+            List<String> out;
+            while ((out = outputPipe.get()) != null)
+                processAndDisplay(out);
+        } catch (InterruptedException e) {
+
+        }
+    }
+
+    /**
+     * Process the list of string for display purposes in the console
+     *
+     * @param output
+     */
+    private void processAndDisplay(List<String> output) {
+
+    }
 }
