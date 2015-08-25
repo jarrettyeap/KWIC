b6aa9933e6c84fca8a0f7ba0830e80e42440da1e
stanley92 <Stanley92@hotmail.com>
2015-08-25 11:02:35 +0800

    Implemented NoiseWordFilter.java

--- src/filters/NoiseWordFilter.java
+++ src/filters/NoiseWordFilter.java
@@ -1,4 +1,27 @@
 package filters;
 
-public class NoiseWordFilter {
+import java.io.EOFException;
+import java.util.Arrays;
+
+public class NoiseWordFilter extends Filter {
+
+	public static final String noiseWords[] = {"is", "the", "of", "and", "as", "a", "after" };
+
+	protected void transform() {
+		while (true) {
+			try {
+				String string = read();
+				String tokens[] = string.split("\\s");
+				
+				if(Arrays.asList(noiseWords).contains(tokens[0].toLowerCase()))
+					continue;
+				
+				write (string);
+				
+			} catch (EOFException e) {
+				write(null);
+				break;
+			}
+		}
+	}
 }
