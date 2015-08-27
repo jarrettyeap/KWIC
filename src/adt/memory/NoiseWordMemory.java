92036cb4b89d26bd26fc8d4487060f8f846d27ac
stanley92 <Stanley92@hotmail.com>
2015-08-27 21:02:55 +0800

    Implemented NoiseWordMemory.java

--- src/adt/memory/NoiseWordMemory.java
+++ src/adt/memory/NoiseWordMemory.java
@@ -1,5 +1,35 @@
 package adt.memory;
 
+import java.util.ArrayList;
+
 public class NoiseWordMemory {
 
+	private ArrayList<String> noiseWordList = new ArrayList<String>();
+	private static NoiseWordMemory noiseWordInstance = null;
+
+	protected NoiseWordMemory() {
+	}
+
+	public static NoiseWordMemory getInstance( ) {
+		if (noiseWordInstance == null) {
+			noiseWordInstance = new NoiseWordMemory();
+		}
+		return noiseWordInstance;
+	}
+
+	public void add(String noiseWord) {
+		noiseWordList.add(noiseWord);
+	}
+	
+	public void delete(int position) {
+		noiseWordList.remove(position);
+	}
+	
+	public String get(int position) {
+		return noiseWordList.get(position);
+	}
+
+	public ArrayList<String> getArrayList() {
+		return noiseWordList;
+	}
 }
