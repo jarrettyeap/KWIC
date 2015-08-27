649e41204ac30b60f884923fb997754ab873ef62
stanley92 <Stanley92@hotmail.com>
2015-08-27 21:02:38 +0800

    Implemented MovieTitleMemory.java

--- src/adt/memory/MovieTitleMemory.java
+++ src/adt/memory/MovieTitleMemory.java
@@ -1,5 +1,36 @@
 package adt.memory;
 
+import java.util.ArrayList;
+
 public class MovieTitleMemory {
 
+	private ArrayList<String> movieTitleList = new ArrayList<String>();
+	private static MovieTitleMemory movieTitleInstance = null;
+	
+	protected MovieTitleMemory() {
+	}
+
+	public static MovieTitleMemory getInstance( ) {
+		if (movieTitleInstance == null) {
+			movieTitleInstance = new MovieTitleMemory();
+		}
+		return movieTitleInstance;
+	}
+	
+	public void add(String title) {
+		movieTitleList.add(title);
+	}
+	
+	public void delete(int position) {
+		movieTitleList.remove(position);
+	}
+	
+	public String get(int position) {
+		return movieTitleList.get(position);
+	}
+	
+	public ArrayList<String> getArrayList() {
+		return movieTitleList;
+	}
+	
 }
