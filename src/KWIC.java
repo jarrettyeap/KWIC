92e12cf57a6d12782f01a96582e5e08da2308d9d
stanley92 <Stanley92@hotmail.com>
2015-08-27 13:55:26 +0800

    Declared Scanner as a global variable, Corrected the options to take in the correct switch case statements

--- src/KWIC.java
+++ src/KWIC.java
@@ -2,19 +2,23 @@ import pipeandfilter.control.MasterControl;
 
 import java.util.Scanner;
 
+import adt.control.AdtControl;
+
 public class KWIC {
-    public static void main(String[] args) {
+    private static Scanner sc;
+
+	public static void main(String[] args) {
         while (true) {
-            Scanner sc = new Scanner(System.in);
+            sc = new Scanner(System.in);
             System.out.println("Choose the following architectures:");
-            System.out.println("1. Pipe and Filter");
-            System.out.println("2. Abstract Data Type");
+            System.out.println("1. Abstract Data Type");
+            System.out.println("2. Pipe and Filter");
             System.out.println("3. Exit");
             int option = sc.nextInt();
 
             switch (option) {
                 case 1:
-                    // Call and pass control to ADT
+                    AdtControl.setup();
                     break;
                 case 2:
                     MasterControl.setup();
