a7b395c006ffa038ae45fdcd5daf55b44ebc4070
stanley92 <Stanley92@hotmail.com>
2015-08-27 21:25:45 +0800

    Improved KWIC.java to detect invalid options

--- src/KWIC.java
+++ src/KWIC.java
@@ -5,27 +5,34 @@ import java.util.Scanner;
 import adt.control.AdtControl;
 
 public class KWIC {
-    private static Scanner sc;
+	private static Scanner sc;
 
 	public static void main(String[] args) {
-        while (true) {
-            sc = new Scanner(System.in);
-            System.out.println("Choose the following architectures:");
-            System.out.println("1. Abstract Data Type");
-            System.out.println("2. Pipe and Filter");
-            System.out.println("3. Exit");
-            int option = sc.nextInt();
+		while (true) {
+			sc = new Scanner(System.in);
+			System.out.println("Choose the following architectures:");
+			System.out.println("1. Abstract Data Type");
+			System.out.println("2. Pipe and Filter");
+			System.out.println("3. Exit");
 
-            switch (option) {
-                case 1:
-                    AdtControl.setup();
-                    break;
-                case 2:
-                    MasterControl.setup();
-                    break;
-                case 3:
-                    System.exit(0);
-            }
-        }
-    }
+			if (sc.hasNextInt()) {
+				int option = sc.nextInt();
+
+				switch (option) {
+				case 1:
+					AdtControl.setup();
+					break;
+				case 2:
+					MasterControl.setup();
+					break;
+				case 3:
+					System.exit(0);
+				default :
+					System.out.println("Invalid Option. Please choose again");
+				}
+			} else {
+				System.out.println("Invalid Option. Please choose again");
+			}
+		}
+	}
 }
