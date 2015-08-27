b40ce77577757ff634db383bda97d364b46e80ef
stanley92 <Stanley92@hotmail.com>
2015-08-27 21:01:22 +0800

    Implemented AdtControl.java

--- src/adt/control/AdtControl.java
+++ src/adt/control/AdtControl.java
@@ -1,9 +1,36 @@
 package adt.control;
 
-public class AdtControl {
+import adt.filter.NoiseFilter;
+import adt.input.Input;
+import adt.memory.MovieTitleMemory;
+import adt.memory.NoiseWordMemory;
+import adt.output.Output;
+import adt.shift.CircularShift;
+import adt.sort.AlphabetSort;
 
+public class AdtControl {
+	
+	static Input input = new Input();
+	static Output output = new Output();
+	static CircularShift circularShift = new CircularShift();
+	static NoiseFilter noiseFilter = new NoiseFilter();
+	static AlphabetSort alphabetSort = new AlphabetSort();
+	static MovieTitleMemory movieTitleArray = MovieTitleMemory.getInstance();
+	static NoiseWordMemory noiseWordArray = NoiseWordMemory.getInstance();
+	
 	public static void setup() {
-		// TODO Auto-generated method stub
+		System.out.println("Please key in the list of movie title: ");
+		System.out.println("Press enter to terminate.");
+		input.addMovieTitle();
 		
+		System.out.println("Please key in the list of noise words: ");
+		System.out.println("Press enter to terminate.");
+		input.addNoiseWord();
+		
+		circularShift.circularizer(movieTitleArray.getArrayList());
+		noiseFilter.noiseWordFilter(circularShift.getArrayList());
+		alphabetSort.alphabetize(noiseFilter.getFilterList());
+		
+		output.print(alphabetSort.getSortedList());
 	}
 }
