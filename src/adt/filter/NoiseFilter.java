168c6a1080ef7bc98f09483c88f57977ea7861e7
stanley92 <Stanley92@hotmail.com>
2015-08-27 21:03:28 +0800

    Implemented NoiseFilter.java

--- src/adt/filter/NoiseFilter.java
+++ src/adt/filter/NoiseFilter.java
@@ -1,5 +1,40 @@
 package adt.filter;
 
+import java.util.ArrayList;
+
+import adt.memory.NoiseWordMemory;
+
 public class NoiseFilter {
 
+	NoiseWordMemory noiseWordArray = NoiseWordMemory.getInstance();
+
+	private ArrayList<String> filterList = new ArrayList<String>();
+	private ArrayList<String> noiseWordList = noiseWordArray.getArrayList();
+
+	public void noiseWordFilter (ArrayList<String> inputArray) {
+
+		filterList = inputArray;
+		
+		String noiseWord;
+		String inputString;
+
+		for (int i=0; i<noiseWordList.size(); i++) {
+			noiseWord = noiseWordList.get(i);
+
+			for (int j=0; j<filterList.size(); j++) {
+				inputString = filterList.get(j);
+				String[] stringArray = inputString.split("\\s");
+
+				if(stringArray[0].equals(noiseWord)) {
+					filterList.remove(j);
+					j--;
+				}
+			}
+		}
+	}
+
+	public ArrayList<String> getFilterList() {
+		return filterList;
+	}
+	
 }
