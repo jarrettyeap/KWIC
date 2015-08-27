81314a9abba581cf2d5379ea3d772b3f64977811
stanley92 <Stanley92@hotmail.com>
2015-08-27 21:02:18 +0800

    Implemented Output.java

--- src/adt/output/Output.java
+++ src/adt/output/Output.java
@@ -1,5 +1,14 @@
 package adt.output;
 
-public class Output {
+import java.util.ArrayList;
 
+public class Output {
+	
+	public void print (ArrayList<String> arrayList) {
+		for (int i=0; i<arrayList.size(); i++) {
+			System.out.println(arrayList.get(i));
+		}
+		System.out.println();
+	}
+	
 }
