17ada1c34715c85f8b26f318d5aafef1a405df4b
stanley92 <Stanley92@hotmail.com>
2015-08-27 21:01:56 +0800

    Implemented Input.java

--- src/adt/input/Input.java
+++ src/adt/input/Input.java
@@ -1,5 +1,38 @@
 package adt.input;
 
+import java.util.Scanner;
+
+import adt.memory.MovieTitleMemory;
+import adt.memory.NoiseWordMemory;
+
 public class Input {
+	
+	private Scanner sc = new Scanner(System.in);
+	MovieTitleMemory movieTitleArray = MovieTitleMemory.getInstance();
+	NoiseWordMemory noiseWordArray = NoiseWordMemory.getInstance();
+	
+	public void addMovieTitle () {
+		String temp;
+		while (sc.hasNextLine()) {
+			temp = sc.nextLine();
+			if (temp.isEmpty()) {
+				break;
+			}
+
+			movieTitleArray.add(temp);
+		}
+	}
+	
+	public void addNoiseWord() {
+		String temp;
+		while (sc.hasNextLine()) {
+			temp = sc.nextLine();
+			if (temp.isEmpty()) {
+				break;
+			}
+
+			noiseWordArray.add(temp);
+		}
+	}
 
 }
