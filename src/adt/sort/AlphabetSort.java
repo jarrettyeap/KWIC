b9384ef36939a3e237835ffad30c956f96044ea4
stanley92 <Stanley92@hotmail.com>
2015-08-27 21:03:07 +0800

    Implemented AlphabetSort.java

--- src/adt/sort/AlphabetSort.java
+++ src/adt/sort/AlphabetSort.java
@@ -1,5 +1,19 @@
 package adt.sort;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class AlphabetSort {
 
+	ArrayList<String> sortedList = new ArrayList<String>();
+	
+	public void alphabetize (ArrayList<String> inputList) {
+		sortedList = inputList;
+		Collections.sort(sortedList);
+	}
+	
+	public ArrayList<String> getSortedList() {
+		return sortedList;
+	}
 }
+
