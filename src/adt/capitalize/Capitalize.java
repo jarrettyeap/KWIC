f61e17d2ec2783f1271e25773543d4ea7a9bae4b
Hooi Tong <hooitongx@gmail.com>
2015-08-30 16:10:55 +0800

    Reformatted after changes for `Capitalize`

--- src/adt/capitalize/Capitalize.java
+++ src/adt/capitalize/Capitalize.java
@@ -4,29 +4,31 @@ import java.util.ArrayList;
 
 public class Capitalize {
 
-	private ArrayList<String> capitalizeList = new ArrayList<String>();
+    private ArrayList<String> capitalizeList = new ArrayList<String>();
 
-	public ArrayList<String> capitalize(ArrayList<String> inputArray, ArrayList<String> noiseArray) {
-		for (int i=0; i<inputArray.size(); i++) {
-			String string = inputArray.get(i);
-			String [] stringArray = string.split("\\s");
-			StringBuilder sb = new StringBuilder();
+    public ArrayList<String> capitalize(ArrayList<String> inputArray,
+        ArrayList<String> noiseArray) {
+        for (int i = 0; i < inputArray.size(); i++) {
+            String string = inputArray.get(i);
+            String[] stringArray = string.split("\\s");
+            StringBuilder sb = new StringBuilder();
 
-			for (int j=0; j<stringArray.length; j++) {
-				stringArray[j] = stringArray[j].substring(0, 1).toUpperCase() + stringArray[j].substring(1);
+            for (int j = 0; j < stringArray.length; j++) {
+                stringArray[j] =
+                    stringArray[j].substring(0, 1).toUpperCase() + stringArray[j].substring(1);
 
-				for (int k=0; k<noiseArray.size(); k++) {
-					if (stringArray[j].equalsIgnoreCase(noiseArray.get(k))) {
-						stringArray[j] = stringArray[j].toLowerCase();
-					}
-				}
-				sb.append(stringArray[j]);
-				sb.append(" ");
-			}
+                for (int k = 0; k < noiseArray.size(); k++) {
+                    if (stringArray[j].equalsIgnoreCase(noiseArray.get(k))) {
+                        stringArray[j] = stringArray[j].toLowerCase();
+                    }
+                }
+                sb.append(stringArray[j]);
+                sb.append(" ");
+            }
 
-			capitalizeList.add(sb.toString().trim());
-		}
+            capitalizeList.add(sb.toString().trim());
+        }
 
-		return capitalizeList;
-	}
+        return capitalizeList;
+    }
 }
