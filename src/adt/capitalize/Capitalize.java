457b5c6db99768e1a0a9fb9b0d6da548eea5436e
stanley92 <Stanley92@hotmail.com>
2015-09-01 22:37:07 +0800

    Edited Capitalized.java to return the array in a different manner

--- src/adt/capitalize/Capitalize.java
+++ src/adt/capitalize/Capitalize.java
@@ -2,33 +2,42 @@ package adt.capitalize;
 
 import java.util.ArrayList;
 
+import adt.memory.NoiseWordMemory;
+
 public class Capitalize {
 
-    private ArrayList<String> capitalizeList = new ArrayList<String>();
+	NoiseWordMemory noiseWordArray = NoiseWordMemory.getInstance();
 
-    public void capitalize(ArrayList<String> inputArray,
-        ArrayList<String> noiseArray) {
-        for (int i = 0; i < inputArray.size(); i++) {
-            String string = inputArray.get(i);
-            String[] stringArray = string.split("\\s");
-            StringBuilder sb = new StringBuilder();
+	private ArrayList<String> noiseWordList = noiseWordArray.getArrayList();
+	private ArrayList<String> capitalizeList = new ArrayList<String>();
 
-            for (int j = 0; j < stringArray.length; j++) {
-                stringArray[j] =
-                    stringArray[j].substring(0, 1).toUpperCase() + stringArray[j].substring(1);
+	public void capitalize(ArrayList<String> inputArray) {
 
-                for (int k = 0; k < noiseArray.size(); k++) {
-                    if (stringArray[j].equalsIgnoreCase(noiseArray.get(k))) {
-                        stringArray[j] = stringArray[j].toLowerCase();
-                    }
-                }
-                sb.append(stringArray[j]);
-                sb.append(" ");
-            }
+		ArrayList<String> arrayCapitalizing = new ArrayList<String>();
 
-            capitalizeList.add(sb.toString().trim());
-        }
-    }
+		for (int i = 0; i < inputArray.size(); i++) {
+			String string = inputArray.get(i);
+			String[] stringArray = string.split("\\s");
+			StringBuilder sb = new StringBuilder();
+
+			for (int j = 0; j < stringArray.length; j++) {
+				stringArray[j] = stringArray[j].substring(0, 1).toUpperCase()
+						+ stringArray[j].substring(1);
+
+				for (int k = 0; k < noiseWordList.size(); k++) {
+					if (stringArray[j].equalsIgnoreCase(noiseWordList.get(k))) {
+						stringArray[j] = stringArray[j].toLowerCase();
+					}
+				}
+				sb.append(stringArray[j]);
+				sb.append(" ");
+			}
+
+			arrayCapitalizing.add(sb.toString().trim());
+		}
+
+		capitalizeList = arrayCapitalizing;
+	}
 
 	public ArrayList<String> getCapitalList() {
 		return capitalizeList;
