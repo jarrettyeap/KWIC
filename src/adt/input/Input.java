4e882c38134694d418ef595d55e2400f6bd1c9ff
stanley92 <Stanley92@hotmail.com>
2015-09-01 22:39:02 +0800

    Standardise Input format in both Architecture

--- src/adt/input/Input.java
+++ src/adt/input/Input.java
@@ -3,48 +3,49 @@ package adt.input;
 import adt.memory.MovieTitleMemory;
 import adt.memory.NoiseWordMemory;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Input {
 
-    private Scanner sc = new Scanner(System.in);
-    MovieTitleMemory movieTitleArray = MovieTitleMemory.getInstance();
-    NoiseWordMemory noiseWordArray = NoiseWordMemory.getInstance();
+	private Scanner sc = new Scanner(System.in);
+	MovieTitleMemory movieTitleArray = MovieTitleMemory.getInstance();
+	NoiseWordMemory noiseWordArray = NoiseWordMemory.getInstance();
 
-    /**
-     * To add a movie title to the system
-     */
-    public void addMovieTitle() {
-        System.out.println("Please key in the list of movie title: ");
-        System.out.println("Press enter to terminate.");
+	/**
+	 * To add a movie title to the system
+	 */
+	public void addMovieTitle() {
+		System.out.println("Please key in the list of movie title: ");
+		System.out.println("Press enter to terminate.");
+		
+		ArrayList<String> inputArray = new ArrayList<String>();
+		String temp;
+		while (sc.hasNextLine()) {
+			temp = sc.nextLine();
+			if (temp.isEmpty()) {
+				break;
+			}
 
-        String temp;
-        while (sc.hasNextLine()) {
-            temp = sc.nextLine();
-            if (temp.isEmpty()) {
-                break;
-            }
+			inputArray.add(temp);
+		}
+		
+		movieTitleArray.setArrayList(inputArray);
+	}
 
-            movieTitleArray.add(temp);
-        }
-    }
+	/**
+	 * To add a noise word to the system
+	 */
+	public void addNoiseWord() {
+		System.out.println("Please enter the list of ignore words (separated by space or comma): ");
+		String ignoreWords = sc.nextLine();
 
-    /**
-     * To add a noise word to the system
-     */
-    public void addNoiseWord() {
-        System.out.println("Please key in the list of noise words: ");
-        System.out.println("Press enter to terminate.");
+		ArrayList<String> ignoreList =
+				new ArrayList<String>(Arrays.asList(ignoreWords.split("(\\s)")));
+		
+		noiseWordArray.setArrayList(ignoreList);
 
-        String temp;
-        while (sc.hasNextLine()) {
-            temp = sc.nextLine();
-            if (temp.isEmpty()) {
-                break;
-            }
-
-            noiseWordArray.add(temp);
-        }
-    }
-
+	}
 }
+
